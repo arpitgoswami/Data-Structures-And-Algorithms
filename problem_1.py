11d2a3c290afc50d8a40d09df8129bb263fad596
arpitgoswami <arpitgoswami@hotmail.com>
2024-03-14 23:16:07 +0530

    Initial Commit

--- problem_1.py
+++ problem_1.py
@@ -1,17 +1,25 @@
 class Solution:
-    def numSubarraysWithSum(self, nums: list[int], goal: int) -> int:
-        res = {}
-        sum = 0
-        count = 0
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
         
-        for i in nums:
-            sum += i
-            if sum - goal in nums:
-                count += res(sum-goal)
-            res[i] = res.get(i, 0) + 1;
+        left, right = 0, len(matrix)-1
+        while left <= right:
+            mid = (left + right) // 2
+            median = matrix[mid]
+            
+            if(median[-1] >= target):
+                for j in median:
+                    if(j == target):
+                        return True
+                else:
+                    continue
+            
+            elif(median[-1] < target):
+                left = mid + 1
+            
+            else:
+                right = mid - 1
         
-        return count
+        return False
 
-                
 s = Solution()
-print(s.numSubarraysWithSum([1,0,1,0,1], 2))
+print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
