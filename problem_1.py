b5f7114e4eef63ff1430ec75f9d82a2042228f26
arpitgoswami <arpitgoswami@hotmail.com>
2024-03-15 22:04:53 +0530

    Initial

--- problem_1.py
+++ problem_1.py
@@ -1,25 +1,18 @@
 class Solution:
-    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        ans = [1] * len(nums)
+        product = 1
         
-        left, right = 0, len(matrix)-1
-        while left <= right:
-            mid = (left + right) // 2
-            median = matrix[mid]
+        for i in range(len(nums)):
+            ans[i] = product
+            product *= nums[i]
             
-            if(median[-1] >= target):
-                for j in median:
-                    if(j == target):
-                        return True
-                else:
-                    continue
-            
-            elif(median[-1] < target):
-                left = mid + 1
-            
-            else:
-                right = mid - 1
+        product = 1
+        for i in range(len(nums)-1,-1,-1):
+            ans[i] *= product
+            product *= nums[i]
         
-        return False
+        return ans
 
 s = Solution()
-print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+print(s.productExceptSelf([1,2,3,4]))
